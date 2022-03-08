207d01dd23c5564fc473888b415fe254b33cc091
shohan-pherones <shohan.sub.56@gmail.com>
2022-03-09 02:04:29 +0600

    conditional

---
+++ conditional.js
@@ -0,0 +1,25 @@
+var isFoodReady = true;
+
+if(isFoodReady == true){
+    console.log('ami khabar khabo');
+}
+
+var iPhonePrice = 70000;
+var myBudget = 15000;
+
+if(iPhonePrice < myBudget){
+    console.log('ami iphone kinmu');
+}
+else if(iPhonePrice > myBudget){
+    console.log('iphone valo na');
+}
+
+var chickenPrice = 180;
+var myMoney = 50;
+
+if(chickenPrice < myMoney){
+    console.log('Chicken khamu');
+}
+else{
+    console.log('chicken kenar taka nai');
+}
