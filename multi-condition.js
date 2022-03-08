0e54e3a8861862b80758653e4ea28d06c36962d8
shohan-pherones <shohan.sub.56@gmail.com>
2022-03-09 02:16:15 +0600

    multi-condition

---
+++ multi-condition.js
@@ -0,0 +1,10 @@
+var gotJob = false;
+var moneySaved = 50000;
+var hasFlat = true;
+
+if(gotJob == true && moneySaved > 200000 || hasFlat == true){
+    console.log('cholo biye kore feli');
+}
+else{
+    console.log('tor kopale biya nai');
+}
