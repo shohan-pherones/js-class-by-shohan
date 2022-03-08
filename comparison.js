9c3bdaf8f3794ebb11ef731843ad726b7f13df30
shohan-pherones <shohan.sub.56@gmail.com>
2022-03-08 22:21:09 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,15 @@
+// console.log(5 > 6);
+// console.log(5 < 6);
+// console.log(5==6);
+// console.log(6==6);
+// console.log(5 != 6);
+// console.log(5 != 5);
+var myLove = 99;
+var herLove = 100;
+// console.log(myLove > herLove);
+// console.log(myLove == herLove);
+// console.log(myLove < herLove);
+console.log(myLove != herLove);
+
+console.log(studyGood && goodPerson); //and
+console.log(isBanker || goodResult); //or
